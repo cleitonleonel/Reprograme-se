c9946e2c002a1166be7f142397f0296dd8fd13b0
cleiton <cleiton.leonel@gmail.com>
2022-10-15 20:14:13 -0300

    Subindo dados da primeira e segunda semanas para o repositório.

---
+++ F1M1_HTML5_e_CSS3/Semana_2/Atividade_Pratica_da_Semana/static/js/controller.js
@@ -0,0 +1,7 @@
+function contate_me() {
+    window.location.href = "https://web.whatsapp.com/send?text=Olá%20tudo%20bem???&phone=+5527995772291"
+}
+
+function send_me_notify() {
+    window.setTimeout('alert("Mensagem enviada com sucesso!!!");window.close();', 2000);
+}
